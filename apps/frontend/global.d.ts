534af7b4bbfabbce4ff9de1dc6290a2b8ef4e4ff
aXenDeveloper <axendeveloper@gmail.com>
2024-11-20 00:16:58 +0100

    refactor!: Recreate project, change vitnode to rest api

--- apps/frontend/global.d.ts
+++ apps/frontend/global.d.ts
@@ -1,10 +1,8 @@
-import core from '@/plugins/core/langs/en.json';
-import admin from '@/plugins/admin/langs/en.json';
-import welcome from '@/plugins/welcome/langs/en.json';
+import type core from '@/plugins/core/langs/en.json';
+import type admin from '@/plugins/admin/langs/en.json';
 import type blog from '@/plugins/blog/langs/en.json';
-// ! === IMPORT ===
 
-type Messages = typeof core & typeof admin  & typeof blog; // ! === MODULE ===
+type Messages = typeof core & typeof admin & typeof blog;
 
 declare global {
   interface IntlMessages extends Messages {}
