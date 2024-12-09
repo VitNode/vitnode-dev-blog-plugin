70572868f619a96ac9a878a10aa75d2e574c5317
aXenDeveloper <axendeveloper@gmail.com>
2024-12-09 15:03:18 +0100

    chore: Update vitnode

--- apps/frontend/src/plugins/admin/templates/categories/item/item.tsx
+++ apps/frontend/src/plugins/admin/templates/categories/item/item.tsx
@@ -2,17 +2,39 @@ import { CategoriesBlogObj } from 'shared/blog/categories';
 import { useTextLang } from 'vitnode-frontend/hooks/use-text-lang';
 
 import { DeleteItemCategoriesBlogAdmin } from './delete/delete';
+import { Link } from 'vitnode-frontend/navigation';
 
 export const ItemCategoriesBlogAdmin = ({
   name,
   id,
+  color,
+  slug,
 }: CategoriesBlogObj['edges'][0]) => {
   const { convertText } = useTextLang();
 
   return (
-    <div className="flex flex-1 items-center justify-between gap-4">
-      {convertText(name)} - {id}
+    <div className="flex flex-1 items-center justify-between gap-2">
+      <div className="flex items-center gap-4">
+        <div
+          className="size-6 rounded-lg bg-[--color-category]"
+          style={
+            {
+              '--color-category': color,
+            } as React.CSSProperties
+          }
+        />
+        <div className="flex flex-col">
+          <span>{convertText(name)}</span>
+          <Link
+            href={`/blog/category/${slug}`}
+            target="_blank"
+            className="text-sm"
+          >{`/blog/category/${slug}`}</Link>
+        </div>
+      </div>
+
       <div>
+        <DeleteItemCategoriesBlogAdmin id={id} name={name} />
         <DeleteItemCategoriesBlogAdmin id={id} name={name} />
       </div>
     </div>
